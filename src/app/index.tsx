a5502a95ab914461abc92f01ecc9201c4b6abd1d
Ilya Azin <martis.azin@gmail.com>
2020-10-20 21:25:22 +0300

    feat(init): restructure
    #1

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./index.scss";
+
+/**
+ * Entry-point приложения
+ */
+const App = () => {
+  return (
+    <div className="gitcli-app">
+      Hello React!
+    </div>
+  );
+}
+
+export default App;
