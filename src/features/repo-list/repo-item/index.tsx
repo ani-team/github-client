00b730b3233093341e0d9119b56e3d6efc3f1870
Adeliya <adeliyanazipova@mail.ru>
2020-11-15 02:22:37 +0300

    fix(profile): style and date format

--- src/features/repo-list/repo-item/index.tsx
+++ src/features/repo-list/repo-item/index.tsx
@@ -7,14 +7,15 @@ type Props = any;
 
 const RepoItem = (props: Props) => {
     const { name, primaryLanguage, updatedAt, url, viewerHasStarred } = props;
+    const hex = primaryLanguage?.color;
 
     return (
         <div className="repo-item">
             <div className="repo-item__info">
                 {/* FIXME: hardcoded, replace to generation by {username}/{reponame} */}
                 <a href={url.replace("https://github.com/", "/")}>{name}</a>
-                <div className="info">
-                    <span>{primaryLanguage?.name}</span>
+                <div className="repo-item__other-info">
+                    <span style={{ color: hex }}>{primaryLanguage?.name}</span>
                     <span>{updatedAt}</span>
                 </div>
             </div>
