233228759b6ff8813f72f0ce86c4dc7237cd7898
Ilya Azin <martis.azin@gmail.com>
2020-11-18 14:37:57 +0300

    feat(search): add header-search-input logic with redirecting
    #13

--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Layout, Input } from "antd";
+import * as qs from "query-string";
 import { Auth } from "features";
 import ImgLogo from "./logo.png";
 import "./index.scss";
 
 const Header = () => {
     const { isAuth } = Auth.useAuth();
+    // !!! FIXME: limit scope of query-params literals
+    // TODO: (wrap in QueryParamProvider) - wrap app with header instead of only content?
+    // const [search] = useQueryParam("q", StringParam);
+    const search = (qs.parse(window.location.search).q as string) || "";
 
     return (
         <Layout.Header className="header">
@@ -15,7 +20,19 @@ const Header = () => {
                     <img className="header__logo" src={ImgLogo} alt="logo" width={48} height={48} />
                     {!isAuth && <span className="gc-app__title text-white m-4">GITHUB-CLIENT</span>}
                 </a>
-                {isAuth && <Input className="header__search" placeholder="Search..." />}
+                {isAuth && (
+                    <Input
+                        className="header__search"
+                        placeholder="Search..."
+                        defaultValue={search}
+                        onKeyDown={({ key, target }) => {
+                            if (key === "Enter") {
+                                // @ts-ignore FIXME: specify types
+                                window.location.replace(`/search?q=${target.value}`);
+                            }
+                        }}
+                    />
+                )}
             </div>
             <Auth.User />
         </Layout.Header>
