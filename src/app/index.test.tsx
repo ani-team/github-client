57189b9f3d8ac4ebd4ff1904ca2bfe718cc246c7
Ilya Azin <martis.azin@gmail.com>
2020-11-20 21:01:18 +0300

    tests: fix test fault
    #63
    https://github.com/ant-design/ant-design/issues/21096

--- src/app/index.test.tsx
+++ src/app/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-// import App from ".";
+import App from ".";
 
 test("renders without crashing", () => {
-    render(<div>Mock</div>);
+    render(<App />);
 });
