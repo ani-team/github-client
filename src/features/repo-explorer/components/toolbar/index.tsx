a3a7da3d0deb37082abd344422c3bed3f894ed48
niyazm524 <niyazm524@gmail.com>
2020-11-21 20:00:05 +0300

    feat(repo-explorer): added clone repo popover

--- src/features/repo-explorer/components/toolbar/index.tsx
+++ src/features/repo-explorer/components/toolbar/index.tsx
@@ -1,13 +1,42 @@
-import React from "react";
-import { Button, Dropdown } from "antd";
+import { CopyOutlined } from "@ant-design/icons";
+import React, { useCallback } from "react";
+import { Button, Dropdown, Input, Popover } from "antd";
 import { RepoIdentity } from "models";
 import BranchesMenu from "../branches-menu";
+import "./index.scss";
 
 type Props = {
     repo: RepoIdentity;
     branches: Array<{ name: string; prefix: string }>;
     activeBranch: string;
 };
+
+function CloneContent({ url }: { url: string }) {
+    const copyUrl = useCallback(() => {
+        const el = document.getElementById("clone-field") as HTMLInputElement | null;
+        if (!el) return;
+        el.focus();
+        el.select();
+        try {
+            document.execCommand("copy");
+        } finally {
+            el.blur();
+        }
+    }, []);
+
+    return (
+        <div className="clone-content">
+            <Input
+                id="clone-field"
+                value={url}
+                onClick={(e) => (e.target as HTMLInputElement).select()}
+                addonAfter={
+                    <Button onClick={copyUrl} icon={<CopyOutlined className="copy-button" />} />
+                }
+            />
+        </div>
+    );
+}
 
 function RepoToolbar({ repo, branches, activeBranch }: Props) {
     return (
@@ -20,7 +49,14 @@ function RepoToolbar({ repo, branches, activeBranch }: Props) {
             >
                 <Button className="branch-dropdown">{activeBranch}</Button>
             </Dropdown>
-            <Button className="clone-btn">Clone</Button>
+            <Popover
+                placement="bottomRight"
+                title="Clone this repository"
+                trigger="click"
+                content={<CloneContent url={`https://github.com/${repo.owner}/${repo.name}.git`} />}
+            >
+                <Button className="clone-btn">Clone</Button>
+            </Popover>
         </div>
     );
 }
