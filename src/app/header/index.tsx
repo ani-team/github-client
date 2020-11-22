89ecdce7617cdef68b38f9ad603822035d0dd510
Ilya Azin <martis.azin@gmail.com>
2020-11-22 03:48:53 +0300

    fix(search): pass all search-config to url from input
    #86

--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -5,12 +5,14 @@ import { Auth } from "features";
 import ImgLogo from "./logo.png";
 import "./index.scss";
 
+const getParams = () => qs.parse(window.location.search) as Record<string, string>;
+
 const Header = () => {
     const { isAuth } = Auth.useAuth();
     // !!! FIXME: limit scope of query-params literals
     // TODO: (wrap in QueryParamProvider) - wrap app with header instead of only content?
     // const [search] = useQueryParam("q", StringParam);
-    const search = (qs.parse(window.location.search).q as string) || "";
+    const search = getParams().q || "";
 
     return (
         <Layout.Header className="header">
@@ -28,8 +30,14 @@ const Header = () => {
                         onKeyDown={({ key, target }) => {
                             // @ts-ignore FIXME: specify types
                             if (key === "Enter" && target.value) {
-                                // @ts-ignore FIXME: specify types
-                                window.location.replace(`/search?q=${target.value}`);
+                                window.location.replace(
+                                    // !!! FIXME: simplify
+                                    `/search?${qs.stringify({
+                                        // @ts-ignore FIXME: specify types
+                                        q: target.value,
+                                        type: getParams().type,
+                                    })}`,
+                                );
                             }
                         }}
                     />
