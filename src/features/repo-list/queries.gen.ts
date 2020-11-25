55897b2fd2356900323fc0cbbe6058972ef98a16
Ilya Azin <martis.azin@gmail.com>
2020-11-25 07:03:39 +0300

    fix(user): tabs results caching
    #98 #95

--- src/features/repo-list/queries.gen.ts
+++ src/features/repo-list/queries.gen.ts
@@ -9,12 +9,13 @@ export type ReposQueryVariables = Types.Exact<{
 }>;
 
 
-export type ReposQuery = { readonly user?: Types.Maybe<{ readonly repositories: { readonly edges?: Types.Maybe<ReadonlyArray<Types.Maybe<{ readonly node?: Types.Maybe<{ readonly id: string, readonly name: string, readonly updatedAt: any, readonly viewerHasStarred: boolean, readonly url: any, readonly primaryLanguage?: Types.Maybe<{ readonly color?: Types.Maybe<string>, readonly name: string }> }> }>>> } }> };
+export type ReposQuery = { readonly user?: Types.Maybe<{ readonly id: string, readonly repositories: { readonly edges?: Types.Maybe<ReadonlyArray<Types.Maybe<{ readonly node?: Types.Maybe<{ readonly id: string, readonly name: string, readonly updatedAt: any, readonly viewerHasStarred: boolean, readonly url: any, readonly primaryLanguage?: Types.Maybe<{ readonly color?: Types.Maybe<string>, readonly name: string }> }> }>>> } }> };
 
 
 export const ReposDocument = gql`
     query Repos($login: String!, $ownerAffiliations: [RepositoryAffiliation]) {
   user(login: $login) {
+    id
     repositories(ownerAffiliations: $ownerAffiliations, first: 50, orderBy: {field: PUSHED_AT, direction: DESC}) {
       edges {
         node {
