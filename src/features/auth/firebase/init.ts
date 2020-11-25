13d59984ed63b7d153e90c5370a8ae029e2b0e09
niyazm524 <niyazm524@gmail.com>
2020-11-25 04:14:09 +0300

    feat(auth): fixed firebase hardcoded values

--- src/features/auth/firebase/init.ts
+++ src/features/auth/firebase/init.ts
@@ -1,15 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// FIXME: hardcoded config
 const firebaseConfig = {
-    apiKey: "AIzaSyABlBQc-tjCRKWwBj8jTTrMiT2M2UKiJpk",
-    authDomain: "github-client-47c49.firebaseapp.com",
-    databaseURL: "https://github-client-47c49.firebaseio.com",
-    projectId: "github-client-47c49",
-    storageBucket: "github-client-47c49.appspot.com",
-    messagingSenderId: "14406286286",
-    appId: "1:14406286286:web:58c7c11c2762d36a55c99f",
+    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE_appId,
 };
 
 firebase.initializeApp(firebaseConfig);
