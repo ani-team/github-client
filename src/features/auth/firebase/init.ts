e76a38de076289a58bf3e95ab1a11fd2822dd7d8
niyazm524 <niyazm524@gmail.com>
2020-11-25 22:12:28 +0300

    feat(auth): refactored to use getEnv in shared

--- src/features/auth/firebase/init.ts
+++ src/features/auth/firebase/init.ts
@@ -1,15 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
-    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
-    databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
-    projectId: process.env.REACT_APP_FIREBASE_projectId,
-    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-    appId: process.env.REACT_APP_FIREBASE_appId,
-};
+import { firebaseConfig } from "shared/get-env";
 
 firebase.initializeApp(firebaseConfig);
 
