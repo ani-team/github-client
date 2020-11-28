083609bc629e7e724e01348f5b84b8ab53d43372
Ilya Azin <martis.azin@gmail.com>
2020-11-28 05:02:13 +0300

    feat(readme): change theme
    #99

--- src/features/repo-explorer/components/readme/code-renderer.tsx
+++ src/features/repo-explorer/components/readme/code-renderer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialLight as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { okaidia as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 type Props = {
     language: string;
