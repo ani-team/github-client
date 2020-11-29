96cc97634a51ab438f9bbd4bb8940eb4f63ebcf9
Ilya Azin <martis.azin@gmail.com>
2020-11-30 01:00:58 +0300

    refactor(mutations): repo-list hooks grouping
    #73

--- src/features/repo-list/hooks.ts
+++ src/features/repo-list/hooks.ts
@@ -1,22 +1,26 @@
-import { notification } from "antd";
+import { alert } from "shared/helpers";
 import * as Queries from "./queries.gen";
+import * as Params from "./params";
 
-const useStarred = (variables?: Queries.ReposQueryVariables) => {
+export const PAGE_SIZE = 30;
+
+/**
+ * @hook star/unstar логика
+ */
+export const useStarring = (variables?: Queries.ReposQueryVariables) => {
     // FIXME: simplify?
     const [addStar] = Queries.useAddStarMutation();
     const [removeStar] = Queries.useRemoveStarMutation();
 
     const handleStarring = (id?: string | null, viewerHasStarred?: boolean) => {
         if (!id) {
-            notification.error({
-                message: "Starring error",
-                description: "Failed to star/unstar repo",
-            });
+            const actionType = viewerHasStarred ? "unstar" : "star";
+            alert.error(`Failed to ${actionType} repo, try later`);
             return;
         }
 
-        const starAction = viewerHasStarred ? removeStar : addStar;
-        starAction({
+        const action = viewerHasStarred ? removeStar : addStar;
+        action({
             variables: { starrableId: id },
             refetchQueries: [{ variables, query: Queries.ReposDocument }],
         });
@@ -27,4 +31,47 @@ const useStarred = (variables?: Queries.ReposQueryVariables) => {
     };
 };
 
-export default useStarred;
+/**
+ * @hook Работа с фильтрацией по affilations, пагинацией
+ */
+export const useFilters = () => {
+    const { tab, setTab, tabEnum } = Params.useTabParam();
+    const { after, before, setCursor } = Params.useCursorParam();
+
+    /**
+     * Обработчик выбора вкладки
+     * @remark Реактивно сбрасываем пагинацию, при смене вкладки
+     */
+    const handleTabClick: typeof setTab = (type) => {
+        setTab(type);
+        setCursor({});
+    };
+
+    /**
+     * Обработчик пагинации
+     * @remark Явно определяем вкладку, чтобы она точно была задана (для соответствия ссылок features/origin)
+     */
+    const handlePaginationClick: typeof setCursor = (pageInfo) => {
+        setTab(tab);
+        setCursor(pageInfo);
+    };
+
+    return {
+        config: {
+            tab,
+            ownerAffiliations: [tabEnum],
+            after,
+            before,
+            /**
+             * @variant (!before, !after) => Первый вход, фетчим первые {PAGE_SIZE}
+             * @variant (after, !before) => След. страница, фетчим след. первые {PAGE_SIZE}
+             * @variant (!after, before) => Пред. страница, фетчим пред. последние {PAGE_SIZE}
+             * @variant (after, before) => (невозможна из-за реализации)
+             */
+            first: (!before && PAGE_SIZE) || undefined,
+            last: (before && PAGE_SIZE) || undefined,
+        },
+        handleTabClick,
+        handlePaginationClick,
+    };
+};
--- src/features/repo-list/index.tsx
+++ src/features/repo-list/index.tsx
@@ -2,75 +2,32 @@ import React from "react";
 import { Repo, Tabs, SimplePagination, Card } from "shared/components";
 import { str, dom } from "shared/helpers";
 import { useReposQuery } from "./queries.gen";
-import * as Params from "./params";
-import useStarred from "./hooks";
+import { useFilters, PAGE_SIZE, useStarring } from "./hooks";
+import { tabsMap } from "./params";
 import "./index.scss";
 
 type Props = {
     username: string;
 };
 
-const PAGE_SIZE = 30;
-
 /**
- * @hook Работа с фильтрацией по affilations, пагинацией
+ * @feature Список репозиториев пользователя
+ * FIXME: rename to UserRepoList? (coz - user as dep)
  */
-const useFilters = () => {
-    const { tab, setTab, tabEnum } = Params.useTabParam();
-    const { after, before, setCursor } = Params.useCursorParam();
-
-    /**
-     * Обработчик выбора вкладки
-     * @remark Реактивно сбрасываем пагинацию, при смене вкладки
-     */
-    const handleTabClick: typeof setTab = (type) => {
-        setTab(type);
-        setCursor({});
-    };
-
-    /**
-     * Обработчик пагинации
-     * @remark Явно определяем вкладку, чтобы она точно была задана (для соответствия ссылок features/origin)
-     */
-    const handlePaginationClick: typeof setCursor = (pageInfo) => {
-        setTab(tab);
-        setCursor(pageInfo);
-    };
-
-    return {
-        config: {
-            tab,
-            ownerAffiliations: [tabEnum],
-            after,
-            before,
-            /**
-             * @variant (!before, !after) => Первый вход, фетчим первые {PAGE_SIZE}
-             * @variant (after, !before) => След. страница, фетчим след. первые {PAGE_SIZE}
-             * @variant (!after, before) => Пред. страница, фетчим пред. последние {PAGE_SIZE}
-             * @variant (after, before) => (невозможна из-за реализации)
-             */
-            first: (!before && PAGE_SIZE) || undefined,
-            last: (before && PAGE_SIZE) || undefined,
-        },
-        handleTabClick,
-        handlePaginationClick,
-    };
-};
-// FIXME: rename to UserRepoList? (coz - user as dep)
 const RepoList = ({ username }: Props) => {
     const { handleTabClick, handlePaginationClick, config } = useFilters();
     const { data, loading, variables } = useReposQuery({
         variables: { login: username, ...config },
     });
     // TODO: transmit id and viewerHasStarred of nodes to handler func
-    const { handleStarring } = useStarred(variables);
+    const { handleStarring } = useStarring(variables);
     const { pageInfo, totalCount = 0, nodes } = data?.user?.repositories || {};
     const length = nodes?.length;
 
     return (
         <div className="repo-list">
             <Tabs className="repo-list__tabs">
-                {Object.keys(Params.tabsMap).map((type) => (
+                {Object.keys(tabsMap).map((type) => (
                     <Tabs.Item
                         key={type}
                         name={str.capitalize(type)}
@@ -88,7 +45,7 @@ const RepoList = ({ username }: Props) => {
                 {length !== 0 ? (
                     data?.user?.repositories.nodes?.map((node) => (
                         <Repo
-                            onClick={() => handleStarring(node?.id, node?.viewerHasStarred)}
+                            onClick={() => handleStarring(null, node?.viewerHasStarred)}
                             key={node?.id}
                             {...node}
                         />
