a9c7a92adf2041f3de1e9473f85577086dd5c487
Ilya Azin <martis.azin@gmail.com>
2020-12-02 00:21:00 +0300

    fix(readme): local links transformation
    #162

--- src/features/repo-explorer/components/readme/index.tsx
+++ src/features/repo-explorer/components/readme/index.tsx
@@ -7,9 +7,42 @@ import "./index.scss";
 type Props = {
     text: string;
     loading: boolean;
+    /**
+     * @remark Для обработки локальных ссылок
+     */
+    repoUrl: string;
+    /**
+     * @remark Для обработки локальных ссылок
+     */
+    branch: string;
 };
 
-const RepoReadme = ({ text, loading }: Props) => {
+const renderers = {
+    code: CodeRenderer,
+};
+
+const RepoReadme = (props: Props) => {
+    const { text, loading, repoUrl, branch } = props;
+    /**
+     * Нормализация внутренних ссылок
+     * @example
+     * transformLocalUri("https://some-url/...")
+     * // => "https://some-url/..."
+     * transformLocalUri("#some-header")
+     * // => "https://github.com/${repo}#some-header"
+     * transformLocalUri("./SOMEFILE.md")
+     * // => "https://github.com/${repo}/blobk/${branch}/SOMEFILE.md"
+     * transformLocalUri("docs/ANOTHER.md")
+     * // => "https://github.com/${repo}/blobk/${branch}/docs/ANOTHER.md"
+     */
+    const transformLocalUri = (uri: string) => {
+        if (uri.startsWith("http")) return uri;
+        if (uri.startsWith("#")) return `https://github.com/${repoUrl}${uri}`;
+        // Если sibling-link - нормализуем
+        const blobUrl = uri.replace("./", "");
+        return `https://github.com/${repoUrl}/blob/${branch}/${blobUrl}`;
+    };
+
     return (
         <div className="repo-readme mt-6">
             {loading && <SkeletonArea />}
@@ -20,7 +53,8 @@ const RepoReadme = ({ text, loading }: Props) => {
                     <Markdown
                         className="repo-readme__markdown p-8 pt-4"
                         allowDangerousHtml
-                        renderers={{ code: CodeRenderer }}
+                        renderers={renderers}
+                        transformLinkUri={transformLocalUri}
                     >
                         {text}
                     </Markdown>
