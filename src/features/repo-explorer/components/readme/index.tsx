f317166b5177b7f7224813c63d485099a6d78149
Ilya Azin <martis.azin@gmail.com>
2020-12-01 23:39:27 +0300

    fix(readme): dividers, title, offsets
    #162

--- src/features/repo-explorer/components/readme/index.tsx
+++ src/features/repo-explorer/components/readme/index.tsx
@@ -14,9 +14,17 @@ const RepoReadme = ({ text, loading }: Props) => {
         <div className="repo-readme mt-6">
             {loading && <SkeletonArea />}
             {text && (
-                <Markdown className="p-6" allowDangerousHtml renderers={{ code: CodeRenderer }}>
-                    {text}
-                </Markdown>
+                <>
+                    {/* TODO: add link */}
+                    <h2 className="repo-readme__title p-4 m-0">README.md</h2>
+                    <Markdown
+                        className="repo-readme__markdown p-8 pt-4"
+                        allowDangerousHtml
+                        renderers={{ code: CodeRenderer }}
+                    >
+                        {text}
+                    </Markdown>
+                </>
             )}
         </div>
     );
