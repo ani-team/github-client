08f220e333a4cf9db9a2ef911a3b79b4b687c3aa
niyazm524 <niyazm524@gmail.com>
2020-12-02 20:40:17 +0300

    feat(repo-explorer): added pass-through links for entries

--- src/features/repo-explorer/components/entries-view/index.tsx
+++ src/features/repo-explorer/components/entries-view/index.tsx
@@ -3,6 +3,9 @@ import dayjs from "dayjs";
 import { List } from "antd";
 import cn from "classnames";
 import { Link } from "react-router-dom";
+import { RepoIdentity } from "../../../../models";
+import { GITHUB_DOMAIN } from "../../../../shared/get-env";
+import { useBranch } from "../../hooks";
 import SkeletonArea from "../skeleton-area";
 
 // FIXME: import as ReactComponent
@@ -12,10 +15,12 @@ import logo from "./placeholder.png";
 import "./index.scss";
 
 type GitFile = { type: string; name: string };
+type GitFileViewProps = GitFile & { repo: RepoIdentity; branch: string };
 type Props = {
     loading?: boolean;
     files: Array<GitFile>;
     className?: string;
+    repo: RepoIdentity;
     lastCommit?: {
         message: string;
         login?: string;
@@ -25,7 +30,8 @@ type Props = {
     };
 };
 
-const EntriesView = ({ loading, files, lastCommit, className }: Props) => {
+const EntriesView = ({ loading, files, lastCommit, className, repo }: Props) => {
+    const { branch } = useBranch(repo);
     return (
         <div className={cn("repo-git-view", className)}>
             {loading && <SkeletonArea />}
@@ -33,21 +39,27 @@ const EntriesView = ({ loading, files, lastCommit, className }: Props) => {
                 <List
                     header={lastCommit && <LastCommitHeader lastCommit={lastCommit} />}
                     dataSource={files}
-                    renderItem={GitFileView}
+                    renderItem={(item) => <GitFileView {...item} repo={repo} branch={branch} />}
                 />
             )}
         </div>
     );
 };
 
-const GitFileView = ({ name, type }: GitFile) => (
-    <List.Item className="repo-git-view__item">
-        <div>
-            <img alt="type" src={type === "tree" ? FolderIcon : FileIcon} />
-            <span>{name}</span>
-        </div>
-    </List.Item>
-);
+const GitFileView = ({ name, type, repo, branch }: GitFileViewProps) => {
+    const baseUrl = `${GITHUB_DOMAIN}${repo.owner}/${repo.name}`;
+    const link = `${baseUrl}/${type}/${branch ? branch + "/" : ""}${name}`;
+    return (
+        <List.Item className="repo-git-view__item">
+            <div>
+                <img alt="type" src={type === "tree" ? FolderIcon : FileIcon} />
+                <a href={link} target="_blank" rel="noopener noreferrer" className="origin-link">
+                    {name}
+                </a>
+            </div>
+        </List.Item>
+    );
+};
 
 const LastCommitHeader = ({ lastCommit }: { lastCommit: Props["lastCommit"] }) => (
     <div className="repo-git-view__last-commit">
--- src/features/repo-explorer/components/index.tsx
+++ src/features/repo-explorer/components/index.tsx
@@ -1,62 +1,13 @@
 import React from "react";
-import { RepoIdentity, BranchIdentity } from "models";
-import {
-    useRepoBranchInfoQuery,
-    useRepoDefaultBranchQuery,
-    RepoBranchInfoQuery,
-} from "../queries.gen";
+import { RepoIdentity } from "models";
+import { useBranch, useRepoDetails } from "../hooks";
+import { useRepoBranchInfoQuery } from "../queries.gen";
 import RepoToolbar from "./toolbar";
 import EntriesView from "./entries-view";
 import RepoReadme from "./readme";
 
 type Props = {
     repo: RepoIdentity;
-};
-
-/**
- * @hook Получение текущей ветки репозитория
- */
-const useBranch = (repo: Props["repo"]) => {
-    const { data } = useRepoDefaultBranchQuery({
-        variables: {
-            name: repo.name,
-            owner: repo.owner,
-        },
-    });
-    const branch = repo.branch || data?.repository?.defaultBranchRef?.name || "master";
-    return { branch };
-};
-
-/**
- * @hook Получение нужных полей по сфетченным данным по репозиторию
- */
-const useRepoDetails = (repoInfo: RepoBranchInfoQuery | undefined) => {
-    const { repository } = repoInfo || {};
-    const branches = (repository?.refs?.nodes || []).filter(
-        (branch): branch is BranchIdentity => !!branch,
-    );
-    const files = Array.from(repository?.object?.entries ?? []).sort((a, b) =>
-        b.type.localeCompare(a.type),
-    );
-    const target = repository?.ref?.target;
-    const lastCommit =
-        (target && {
-            message: target.messageHeadline,
-            login: target.author?.user?.login,
-            avatarUrl: target.author?.user?.avatarUrl,
-            name: target.author?.name,
-            date: target.author?.date,
-        }) ||
-        undefined;
-
-    // Приходится фетчить файл по двум вариантам наименования, т.к. GitHub не умеет в insensitive case =(
-    const readme = repository?.contentLower?.text || repository?.contentUpper?.text || "";
-    return {
-        branches,
-        files,
-        lastCommit,
-        readme,
-    };
 };
 
 const Explorer = ({ repo }: Props) => {
@@ -77,7 +28,13 @@ const Explorer = ({ repo }: Props) => {
     return (
         <div>
             <RepoToolbar repo={repo} branches={branches} activeBranch={branch} />
-            <EntriesView files={files} lastCommit={lastCommit} loading={loading} className="mt-3" />
+            <EntriesView
+                files={files}
+                lastCommit={lastCommit}
+                loading={loading}
+                repo={repo}
+                className="mt-3"
+            />
             <RepoReadme text={readme} loading={loading} repoUrl={repoUrl} branch={branch} />
         </div>
     );
