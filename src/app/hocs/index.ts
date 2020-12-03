e7cf1567f1582e4677d3e92a1a80f2b0da0521a8
Ilya Azin <martis.azin@gmail.com>
2020-12-03 23:56:22 +0300

    fix(app): popover scroll container position
    #162

--- src/app/hocs/index.ts
+++ src/app/hocs/index.ts
@@ -1,8 +1,9 @@
 import withApollo from "./with-apollo";
 import withRouter from "./with-router";
+import withAntd from "./with-antd";
 
 /**
  * @hoc Инициализирующая логика приложения
  * FIXME: Потом какой-нибудь `compose` метод заинсталлим откуда-нить и покрасивше будет
  */
-export const withHocs = (component: Component) => withRouter(withApollo(component));
+export const withHocs = (component: Component) => withAntd(withRouter(withApollo(component)));
