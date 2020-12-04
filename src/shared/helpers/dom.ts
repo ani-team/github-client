3f87ac9aa5f299a6445c3482e7da1178932198b1
Ilya Azin <martis.azin@gmail.com>
2020-12-04 11:53:03 +0300

    hotfix(search): autoscroll top

--- src/shared/helpers/dom.ts
+++ src/shared/helpers/dom.ts
@@ -2,7 +2,8 @@
  * Временная реализация скроллинга к верху страницы
  * @remark Используется при пагинации
  * !!! FIXME: temp, resolve better later (by anchors / overflow / ref / scrollHandler / window patching / ...)
+ * !!! FIXME: resolve on withAntd level? (with getParentContainer)
  */
 export const scrollToTop = () => {
-    document.querySelector(".gc-app")?.scrollTo({ top: 0, behavior: "smooth" });
+    document.querySelector(".ant-layout-content")?.scrollTo({ top: 0, behavior: "smooth" });
 };
